2022fffdfab780c7577652386a9f46cad4882d42
KKarani <kkarani1@outlook.com>
2022-09-01 04:05:15 +0000

    Update RunWinProcess.ps1

--- RunWinProcess.ps1
+++ RunWinProcess.ps1
@@ -1,2 +1,4 @@
 # Script to start a process on a specific file path. Replace X with filepath folders and replace Y with executable name.
 Start-Process -Filepath "C:\X\X\X\X\Y.exe"
+Write-Output "Y.exe run start time: $(Get-Date)"
+Write-Output "Y.exe run approximate end time: $((Get-Date).AddMinutes(+5))"
